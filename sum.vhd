05aa8f7d0de0161d7a454539151e431b86ce7b40
khawla bougherara <125137694+khawlabgr@users.noreply.github.com>
2025-04-27 17:53:21 +0200

    Update sum.vhd

--- sum.vhd
+++ sum.vhd
@@ -1,31 +1,17 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    18:41:33 12/08/2024 
--- Design Name: 
--- Module Name:    sum - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- Engineer: Khawla BOUGHERARA
+-- Create Date:    11:34:03 12/08/2024 
+-- Module Name:   sum - Behavioral 
+-- Project Name: Artificial-Neural-Network-FPGA-Design-in-VHDL
+-- Target Devices: FPGA
+-- Tool versions: XILINX ISE
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+
 
 entity sum is
     Port ( sem : in  STD_LOGIC_VECTOR (15 downto 0);
